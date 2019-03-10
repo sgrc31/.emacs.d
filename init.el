67734c93e20042865012c8061ab48bf3a5c78c6f
Simone Guercio <s.guercio@gmail.com>
2019-03-10 18:19:10 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/lab/.ordini.org" "~/lab/.labnotes.org" "~/.notes.org")))
+ '(package-selected-packages
+   (quote
+    (sass-mode pdf-tools ido-vertical-mode auctex web-mode powerline org markdown-mode emmet-mode elpy base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
